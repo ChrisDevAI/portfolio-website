c286964ddf154784f00b73ece8ed0ffde5afd39d
ChrisDevAI <196393949+ChrisDevAI@users.noreply.github.com>
2026-03-30 02:14:21 -0400

    Add vercel analytics support

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 // pages/_app.js
-import '@/styles/globals.css';
-import Layout from '@/components/Layout';
+
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
